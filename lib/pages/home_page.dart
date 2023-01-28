4c1e64b387cd2b9790b5f201d18bbc82438f4d7c
Madan <madangopal16072000@gmail.com>
2023-01-28 18:23:19 +0530

    day14

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,16 +20,46 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+          ),
           itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
+            final item = dummyList[index];
+            return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: GridTile(
+                  header: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                      ),
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        item.name,
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  child: Image.network(item.image),
+                  footer: Text(item.price.toString()),
+                ));
           },
+          itemCount: dummyList.length,
         ),
       ),
       drawer: const MyDrawer(),
     );
   }
 }
+
+
+// ListView.builder(
+//           itemCount: dummyList.length,
+//           itemBuilder: (context, index) {
+//             return ItemWidget(
+//               item: dummyList[index],
+//             );
+//           },
+//         )
