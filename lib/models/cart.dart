6490149a6d27ca51e87d425a05ea46735a66fc54
Madan <madangopal16072000@gmail.com>
2023-02-06 16:42:27 +0530

    day21 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,29 @@
+import 'package:first_app/models/catalog.dart';
+
+class CartModel {
+  CatalogModel _catalog;
+
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // Add Item
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  // Remove Item
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
